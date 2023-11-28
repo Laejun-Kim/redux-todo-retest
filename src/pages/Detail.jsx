61e070cf02b5ebc2b6e6deb57917c76666e26007
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2023-11-28 11:33:40 +0900

    second commit

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,29 @@
+import { useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+
 const Detail = () => {
-  return <div></div>;
+  const todos = useSelector((state) => state.todos);
+  const location = useLocation();
+  const todoId = location.pathname.slice(1);
+  const navigate = useNavigate();
+  const [targetTodo] = todos.filter((todo) => todo.id === todoId);
+  const { title, contents, isDone } = targetTodo;
+  console.log(targetTodo);
+  console.log(todos);
+  return (
+    <>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        이전 화면으로
+      </button>
+      <p>제목 : {title}</p>
+      <p>내용 : {contents}</p>
+      <p>완료여부 : {isDone ? "완료" : "미완료"}</p>
+    </>
+  );
 };
 
 export default Detail;
